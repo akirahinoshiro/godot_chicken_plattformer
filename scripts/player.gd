0f00528f06a64321a7e0e152eaca8e2129f74939
akira hinoshiro <akirahinoshiro@gmail.com>
2020-03-27 22:10:24 +0100

    you only die once!

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,7 @@ onready var cam = $CamBase
 onready var anim = $chickenV2/AnimationPlayer
 
 var y_velo = 0
+var dead = false
 
 func _ready():
 	anim.get_animation("Idle1").set_loop(true)
@@ -24,47 +25,45 @@ func _ready():
 		#rotation_degrees.y -= event.relative.x * H_LOOK_SENS
 
 func _physics_process(delta):
-	var move_vec = Vector3()
-	if Input.is_action_pressed("move_forwards"):
-		move_vec.z -= 1
-	if Input.is_action_pressed("move_backwards"):
-		move_vec.z += 1
-	if Input.is_action_pressed("move_right"):
-		move_vec.x += 1
-	if Input.is_action_pressed("move_left"):
-		move_vec.x -= 1
-	move_vec = move_vec.normalized()
-	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-	move_vec *= MOVE_SPEED
-	move_vec.y = y_velo
-	move_and_slide(move_vec, Vector3(0, 1, 0))
+	if !dead:
+		var move_vec = Vector3()
+		if Input.is_action_pressed("move_forwards"):
+			move_vec.z -= 1
+		if Input.is_action_pressed("move_backwards"):
+			move_vec.z += 1
+		if Input.is_action_pressed("move_right"):
+			move_vec.x += 1
+		if Input.is_action_pressed("move_left"):
+			move_vec.x -= 1
+		move_vec = move_vec.normalized()
+		move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
+		move_vec *= MOVE_SPEED
+		move_vec.y = y_velo
+		move_and_slide(move_vec, Vector3(0, 1, 0))
 	
-	var grounded = is_on_floor()
-	y_velo -= GRAVITY
-	var just_jumped = false
-	if grounded and Input.is_action_just_pressed("jump"):
-		just_jumped = true
-		y_velo = JUMP_FORCE
-	if grounded and y_velo <= 0:
-		y_velo = -0.1
-	if y_velo < -MAX_FALL_SPEED:
-		y_velo = -MAX_FALL_SPEED
+		var grounded = is_on_floor()
+		y_velo -= GRAVITY
+		var just_jumped = false
+		if grounded and Input.is_action_just_pressed("jump"):
+			just_jumped = true
+			y_velo = JUMP_FORCE
+		if grounded and y_velo <= 0:
+			y_velo = -0.1
+		if y_velo < -MAX_FALL_SPEED:
+			y_velo = -MAX_FALL_SPEED
 	
-	if just_jumped:
-		play_anim("jump")
-	elif grounded:
-		if move_vec.x == 0 and move_vec.z == 0:
-			play_anim("Idle1")
-		else:
-			play_anim("Walk")
-	if (global_transform.origin.y < -100):
-		death()
+		if just_jumped:
+			play_anim("jump")
+		elif grounded:
+			if move_vec.x == 0 and move_vec.z == 0:
+				play_anim("Idle1")
+			else:
+				play_anim("Walk")
+		if (global_transform.origin.y < -100 and !dead):
+			play_anim("Dying")
+			dead = true
 
 func play_anim(name):
 	if anim.current_animation == name:
 		return
 	anim.play(name)
-	
-func death():
-	play_anim("Dying")
-	
